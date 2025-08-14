6af7c76172f221c359fe62251a8383abddcc440a
Charl Concepcion <charliecatxph@gmail.com>
2025-08-14 13:42:11 +0800

    add logs authGate

--- authentication/authGate.ts
+++ authentication/authGate.ts
@@ -24,6 +24,7 @@ export const authGate = async (ctx: GetServerSidePropsContext) => {
   const resolvedUrl = ctx.resolvedUrl.split("?")[0];
 
   try {
+    console.log("INIT");
     const res = await axios.post(
       `${absoluteServerUrl}/api/user-rehydration`,
       {},
@@ -33,6 +34,7 @@ export const authGate = async (ctx: GetServerSidePropsContext) => {
         },
       }
     );
+    console.log("AUTH OK");
 
     const userData = {
       ...(jwt.decode(res.data.token) as object),
@@ -83,6 +85,7 @@ export const authGate = async (ctx: GetServerSidePropsContext) => {
       },
     };
   } catch (e) {
+    console.log("ERROR IN AUTHENTICATION", e);
     if (protectedRoutes.some((route) => route === resolvedUrl)) {
       return {
         redirect: {
